1d92eb5944165b013046cc446ee8233d79e29093
Jorge Medina <jlmedinamacias@gmail.com>
2024-06-04 15:55:54 -0700

    Connect to a PostgreSQL databasefrom a JavaScript app using node-postgres library.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,34 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx"
+});
+
+// Takes arguments from the command line terminal
+const args = process.argv.slice(2);
+
+pool
+  .query(
+    `
+SELECT
+  teachers.name as teacher,
+  cohorts.name as cohort,
+  count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${args[0]}'
+GROUP BY teacher, cohort
+ORDER BY teacher;
+    `
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(`The teacher ${user.teacher} had ${user.total_assistances} assinstances in the ${user.cohort} cohort.`);
+    });
+  })
+  .catch((err) => console.log("query error", err.stack));
